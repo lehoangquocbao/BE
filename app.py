e567f1c5bcc8903a53c33ec38541cc20a2a5604c
LeHoangQuocBao <lehoangquocbao06@gmail.com>
2025-07-29 22:25:59 +0700

    Thêm API đặt lịch và xem lịch

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+appointments = []
 
-@app.route('/')
-def home():
-    return "Hello Word!"
+@app.route('/appointments', methods=['POST'])
+def create_appointment():
+    data = request.get_json
+    appointments.append(data)
+    return jsonify({"message": "Appointment created"}), 201
+
+@app.route('/appointments', methods=['GET'])
+def get_appointments():
+    return jsonify(appointments), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
+
