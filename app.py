e3105ae6b7362f8e7b1eec5567f7c021618e7e84
LeHoangQuocBao <lehoangquocbao06@gmail.com>
2025-07-30 19:35:44 +0700

    Initial commit

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask, request, jsonify
+from urllib.request import request_host
+
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
+from flask import request
 
 app = Flask(__name__)
-appointments = []
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+patients =[]
+@app.route("/patients", methods=['GET'])
+def get_patients():
+    return jsonify(patients)
 
-@app.route('/appointments', methods=['POST'])
-def create_appointment():
-    data = request.get_json
-    appointments.append(data)
-    return jsonify({"message": "Appointment created"}), 201
+@app.route("/patients", methods=['POST'])
+def add_patients():
+    data= request.get_json()
+    patient={
+        "id":len(patients)+1,
+        "name":data.get("name","anonymus"),
+        "age":data.get("age","None"),
+    }
+    patients.append(patient)
+    return jsonify(patients), 201
 
-@app.route('/appointments', methods=['GET'])
-def get_appointments():
-    return jsonify(appointments), 200
-
+#start backend
 if __name__ == '__main__':
     app.run(debug=True)
-
