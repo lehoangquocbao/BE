0170769eadf77cbb4c8e5e4adead6ef314d6418d
HOPHAMTHANHPHUC <phamhophuc123456@gmail.com>
2025-08-21 19:10:38 +0700

    Update db_sqlserver.py

--- db_sqlserver.py
+++ db_sqlserver.py
@@ -3,8 +3,8 @@ from models.class_appointment import db
 # Hàm khởi tạo kết nối SQL Server cho Flask app
 # Sử dụng trong app.py hoặc main entrypoint
 
-def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///be_database.db'
+def init_sqlserver(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:Aa123456@127.0.0.1:1433/master?driver=ODBC+Driver+17+for+SQL+Server'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
