9588de6fceeaf7e4632ff4d8358624c68430cd70
truonglamtien206 <tientl2402@ut.edu.vn>
2025-08-23 01:05:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 from models.class_appointment import db, Appointment, Reminder, AnonymousToken
 from db_sqlserver import init_sqlserver
 from appointment_api import appointment_bp  # Import blueprint API
-
+from admin_controller import admin_bp
+from report_controller import report_bp
+from reminder_controller import reminder_bp
 app = Flask(__name__)
-
+app.register_blueprint(admin_bp)
+app.register_blueprint(report_bp)
+app.register_blueprint(reminder_bp)
 # Khởi tạo kết nối SQL Server
 init_sqlserver(app)
 
